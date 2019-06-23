c25f3ce859fe3a9a24fcb4655a0dd1281afcdd40
Doug Larrick <doug@parkercat.org>
2019-06-23 09:16:15 -0400

    Add day-of-week; fix for off overriding manual changes Pylint fixes Fix for switching out of 'dry' mode not setting temperature Only log action if taking it

--- seasons.py
+++ seasons.py
@@ -1,42 +1,53 @@
-# Seasons.py -- Hass helper python_script to turn a Climate entity into a smart thermostat, with
-# support for multiple thermostats each having their own schedule.
+# Seasons.py -- Hass helper python_script to turn a Climate entity into a smart
+# thermostat, with support for multiple thermostats each having their own
+# schedule.
 #
 # INPUTS:
 # * climate_unit: The Climate entity to be controlled
-# * global_mode: an entity whose state is the desired global climate mode (usually an input_select)
-# * at_home_sensor: an entity that represents whether anyone is home (usually a binary_sensor)
-# * from_timer: whether the script was triggered by timer. If true, only changes to the new mode /
-#   setpoint within the first 15 minutes of each schedule, so that manual overrides stick until
-#   the next schedule.
+# * global_mode: an entity whose state is the desired global climate mode
+#   (usually an input_select)
+# * at_home_sensor: an entity that represents whether anyone is home (usually
+#   a binary_sensor)
+# * from_timer: whether the script was triggered by timer. If true, only changes
+#   to the new mode / setpoint within the first 15 minutes of each schedule, so
+#   that manual overrides stick until the next schedule.
 #
-# The last input is the 'seasons' dictionary as defined below, which defines the scheduled behavior
-# for each global mode / climate unit combination. It's a dictionary keyed by a tuple of
-# global mode and climate unit. Each entry is a list of schedules, where each schedule has the
-# following fields:
+# The last input is the 'seasons' dictionary as defined below, which defines the
+# scheduled behavior for each global mode / climate unit combination. It's a
+# dictionary keyed by a tuple of global mode and climate unit. Each entry is a
+# list of schedules, where each schedule has the following fields:
 # * title: Used only for logging and to help you find the right entry for edits
-# * time_on / time_off (optional): Start and stop of this schedule, 24-hour hours:minutes. If not
-#   given, this schedule is always active (though see window and if_away/if_home below)
-# * operation: The operating mode for this schedule, one of the modes supported by your climate
-#   entity.
-# * setpoint (optional): The desired temperature for this schedule. Some modes (e.g. 'dry'
-#   dehumidifaction) don't require a setpoint so it's optional
-# * window (optional): If given, if this entity's state is 'on' (i.e. the given window is open),
-#   the schedule will act as if its operation mode is 'off'. This is so you don't attempt to
-#   heat/cool the great outdoors if you left the window open for some fresh air.
-# * if_away / if_home (optional): If present, this schedule will only apply if the at_home_sensor
-#   state matches (true meaning someone is home). If no at_home_sensor is given, these are both
-#   always false.
+# * time_on / time_off (optional): Start and stop of this schedule, 24-hour
+#   hours:minutes. If not given, this schedule is always active (though see
+#   window and if_away/if_home below)
+# * days: (optional): String defining days of week this schedule is active.
+#   Seven characters, dash or dot if not active, any other character if active.
+#   You can use 0123456 or MTWTFSS or whatever you like. Monday is first,
+#   following Python datetime convention.
+# * operation: The operating mode for this schedule, one of the modes supported
+#   by your climate entity.
+# * setpoint (optional): The desired temperature for this schedule. Some modes
+#   (e.g. 'dry' dehumidifaction) don't require a setpoint so it's optional
+# * window (optional): If given, if this entity's state is 'on' (i.e. the given
+#   window is open), the schedule will act as if its operation mode is 'off'.
+#   This is so you don't attempt to heat/cool the great outdoors if you left the
+#   window open for some fresh air.
+# * if_away / if_home (optional): If present, this schedule will only apply if
+#   the at_home_sensor state matches (true meaning someone is home). If no
+#   at_home_sensor is given, these are both always false.
 #
-# Put this script in <config>/python_scripts (create the directory if needed) and activate it
-# as described at https://www.home-assistant.io/components/python_script/ .
-# You should set up automations to call service python_script.seasons for each relevant climate
-# unit for the each of the following events:
+# Put this script in <config>/python_scripts (create the directory if needed)
+# and activate it as described at
+# https://www.home-assistant.io/components/python_script/ .
+# You should set up automations to call service python_script.seasons for each
+# relevant climate unit for the each of the following events:
 # * your global_mode entity changes (all climate units)
 # * your at_home_sensor changes (all climate units)
 # * your window sensor(s) change(s) (relevant climate units)
-# * on a time_interval, suggested every 15 minutes. (all climate units). This interval is the
-#   resolution of your scheduled changes, so make it more or less frequent as required; you may
-#   need to change the 'interval' variable below as well.
+# * on a time_interval, suggested every 15 minutes. (all climate units). This
+#   interval is the resolution of your scheduled changes, so make it more or
+#   less frequent as required; you may need to change the 'interval' variable
+#   below as well.
 
 seasons = {
     ('Normal Summer', 'climate.master_br'): [
@@ -174,25 +185,25 @@ def is_time_between(begin_time, end_time, check_time):
     return check_time >= begin_time or check_time <= end_time
 
 def time_offset(orig_time, offset):
-    hr = orig_time.hour
-    min = orig_time.minute
-    min = min + offset
-    if min < 0:
-        hr = hr - 1
-        min = min + 60
-    if min > 60:
-        hr = hr + 1
-        min = min - 60
-    if hr < 0:
-        hr = hr + 24
-    if hr > 24:
-        hr = hr - 24
-    return datetime.time(hour=hr, minute=min)
+    hour = orig_time.hour
+    minute = orig_time.minute
+    minute = minute + offset
+    if minute < 0:
+        hour = hour - 1
+        minute = minute + 60
+    if minute > 60:
+        hour = hour + 1
+        minute = minute - 60
+    if hour < 0:
+        hour = hour + 24
+    if hour > 24:
+        hour = hour - 24
+    return datetime.time(hour=hour, minute=minute)
 
 interval = 15
 
 climate_unit = data.get('climate_unit', 'climate.master_br')
-current_mode = hass.states.get(data.get('global_mode', 'input_select.climate_mode')).state
+current_mode = hass.states.get(data.get('global_mode')).state
 from_timer = data.get('from_timer', False)
 at_home_sensor = data.get('at_home_sensor')
 is_home = False
@@ -203,6 +214,7 @@ if at_home_sensor:
 
 current_operation = hass.states.get(climate_unit).state
 now = datetime.datetime.now().time()
+today = datetime.datetime.now().weekday()
 
 key = (current_mode, climate_unit)
 schedules = seasons.get(key)
@@ -217,6 +229,7 @@ else:
     desired_operation = None
     setpoint = None
     title = None
+    most_recent_end = None
     for schedule in schedules:
         time_on_str = schedule.get('time_on')
         time_off_str = schedule.get('time_off')
@@ -226,29 +239,42 @@ else:
             time_on = datetime.datetime.strptime(time_on_str, '%H:%M').time()
         if time_off_str:
             time_off = datetime.datetime.strptime(time_off_str, '%H:%M').time()
-        in_interval = ((not time_on) or (not time_off) or
-                       is_time_between(time_on, time_off, now))
+
+        day_match = true
+        days = schedule.get('days')
+        if days and len(days) is 7:
+            day_match = days[today] != '-' and days[today] != '.'
+
+        in_interval = day_match and (((not time_on) or (not time_off) or
+                                      is_time_between(time_on, time_off, now)))
+
         home_away_match = True
         if schedule.get('if_home'):
             home_away_match = is_home
         if schedule.get('if_away'):
             home_away_match = not is_home
 
+        if home_away_match and day_match and (not in_interval) and time_off:
+            if not most_recent_end or is_time_between(
+                    most_recent_end, now, time_off):
+                most_recent_end = time_off
+
         if in_interval and home_away_match:
+            # When we get here, we have schedules for this unit and
+            # global mode and we're in this schedule's interval.
+            # We will obey this schedule and ignore subsequent matches
             if not time_on:
                 time_on = datetime.datetime.strptime('00:00', '%H:%M').time()
             first_interval_start = time_on
             first_interval_end = time_offset(time_on, interval)
-            in_first_interval = is_time_between(first_interval_start, first_interval_end, now)
+            in_first_interval = is_time_between(first_interval_start,
+                                                first_interval_end, now)
             window_open = False
             if schedule.get('window'):
                 window_open = hass.states.get(schedule['window']).state is 'on'
             else:
                 window_open = False
 
-            # When we get here, we have schedules for this unit and
-            # global mode and we're in this schedule's interval.
-            # We will obey this schedule and ignore subsequent matches
             decided = False
             matched = True
 
@@ -276,18 +302,28 @@ else:
                 title = schedule.get('title')
                 decided = True
             break
-        
+
     if not matched:
         # If no schedules matched, turn off
-        # FIXME: only do this just past the most recent schedule period
-        turn_off = True
-        title = 'Default (Off)'
+        just_past_interval = False
+        if most_recent_end:
+            just_past_end = time_offset(most_recent_end, interval)
+            just_past_interval = is_time_between(
+                most_recent_end, just_past_end, now)
+        if (not from_timer) or just_past_interval:
+            turn_off = True
+            title = 'Default (Off)'
 
     if turn_off:
         desired_operation = 'off'
 
-    logger.info("Setting {} to mode {} target {} from schedule {}".format(
-        climate_unit, desired_operation, setpoint, title))
+    if desired_operation:
+        logger.info("Setting {} to mode {} target {} from schedule {}".format(
+            climate_unit, desired_operation, setpoint, title))
+        service_data = {
+            "entity_id": climate_unit,
+            "operation_mode": desired_operation}
+        hass.services.call('climate', 'set_operation_mode', service_data, False)
 
     if setpoint and desired_operation:
         service_data = {
@@ -295,9 +331,3 @@ else:
             "temperature": setpoint,
             "operation_mode": desired_operation}
         hass.services.call('climate', 'set_temperature', service_data, False)
-
-    if desired_operation:
-        service_data = {
-            "entity_id": climate_unit,
-            "operation_mode": desired_operation}
-        hass.services.call('climate', 'set_operation_mode', service_data, False)
